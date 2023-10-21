f31e69cf83abf8d27cc5aac1ea22aeb8bef925ef
tom-parish <100394280+tom-parish@users.noreply.github.com>
2023-10-21 12:35:24 +1100

    Updated Ply files for Panda Bot
    Updated Ply files for Panda Bot

--- Assignment 2/@Panda/Panda.m
+++ Assignment 2/@Panda/Panda.m
@@ -42,7 +42,7 @@ classdef Panda < RobotBaseClass
             link(5) = Link('d',0.384,'a',-0.0825,'alpha',-pi/2,'offset',0,'qlim', [-2.8065,2.8065]);
             link(6) = Link('d',0,'a',0,'alpha',pi/2,'offset',0,'qlim', [0.5445,4.5169]);
             link(7) = Link('d',0,'a',0.088,'alpha',pi/2,'offset',0,'qlim', [-3.0159,3.0159]);
-            link(8) = Link('d',1.07,'a',0,'alpha',0,'offset',0,'qlim', [0,0]);
+       %     link(8) = Link('d',1.07,'a',0,'alpha',0,'offset',0,'qlim', [0,0]);
         
             self.model = SerialLink(link,'name',self.name);
         end      
